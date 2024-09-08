198909e7b73a918b8e6b23a091522433e5ba4cc9
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-09-09 00:31:57 +0530

    formula for sales revenue has been added

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,4 @@
-from src.models import Data, Formula, ResultBody
+from src.models import Data, Formula, ResultBody, Inputs
 from typing import Any, List, Dict
 
 
@@ -15,18 +15,24 @@ class FormulaExecutor:
         except Exception as e:
             raise ValueError(f"Invalid expression: {expression}. Error: {str(e)}")
 
+    def _execute_formula_for_given_data(
+        self, expression: str, list_of_incoming_inputs: List[Inputs], given_data: Data
+    ) -> Any:
+        input_variables = {input_variable.varName: getattr(given_data, input_variable.varName) for input_variable in list_of_incoming_inputs}
+        expression_output = self._safe_eval(expression, input_variables)
+        return expression_output
+
     def perform_formula_execution(self) -> List[ResultBody]:
-        result_body = ResultBody()
-        for formula in self.formula_list:
-            expression = formula.expression
-            inputs = formula.inputs
-            output_var = formula.outputVar
-            result_list = []
+        result_body: ResultBody = ResultBody()
+        for formula_considered in self.formula_list:
+            expression: str = formula_considered.expression
+            list_of_incoming_inputs: List[Inputs] = formula_considered.inputs
+            output_variable: str = formula_considered.outputVar
+            output_list: List[float] = []
+            
             for data in self.data_list:
-                result_body = ResultBody()
-                variables = {input_var.varName: getattr(data, input_var.varName) for input_var in inputs}
-                result = self._safe_eval(expression, variables)
-                result_list.append(result)
-            setattr(result_body, output_var, result_list)
-        
+                formula_output = self._execute_formula_for_given_data(expression, list_of_incoming_inputs, data)
+                output_list.append(formula_output)
+            setattr(result_body, output_variable, output_list)
         return result_body
+
